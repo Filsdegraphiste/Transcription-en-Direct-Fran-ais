4ebc888d4f51eea6628998ae057f69080df6237a
Lionel Taurus <89390517+Filsdegraphiste@users.noreply.github.com>
2024-09-08 13:04:17 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ const tailleTexteSlider = document.getElementById('tailleTexte');
 let recognition;
 let enCoursDeReconnaissance = false; // Track if recognition is active
 let texteFinal = ''; // Store the final results
-let pauseTimeout;  // Timeout to track pauses
-const PAUSE_DURATION = 1000; // 1 seconds
 
 // Vérification de la compatibilité du navigateur avec SpeechRecognition API
 if ('webkitSpeechRecognition' in window) {
@@ -24,8 +22,6 @@ if ('webkitSpeechRecognition' in window) {
 
     recognition.onresult = function (event) {
         let texteIntermediaire = ''; // Temporary holder for interim results
-        clearTimeout(pauseTimeout); // Reset the pause timer on new speech input
-
         for (let i = event.resultIndex; i < event.results.length; ++i) {
             const result = event.results[i];
             if (result.isFinal) {
@@ -34,17 +30,8 @@ if ('webkitSpeechRecognition' in window) {
                 texteIntermediaire += result[0].transcript;
             }
         }
-
         transcriptionBox.textContent = texteFinal + ' ' + texteIntermediaire;
         scrollToEnd(); // Ensure the latest text is visible by scrolling
-
-        // Set a timeout to add a period after 2 seconds of silence
-        pauseTimeout = setTimeout(() => {
-            if (!texteFinal.endsWith('.')) {
-                texteFinal += '.';
-                transcriptionBox.textContent = texteFinal;  // Update the display with the period
-            }
-        }, PAUSE_DURATION);
     };
 
     recognition.onend = function () {
@@ -81,7 +68,7 @@ if ('webkitSpeechRecognition' in window) {
 
     // Change text size
     tailleTexteSlider.addEventListener('input', function () {
-        transcriptionBox.style.fontSize = `${tailleTexteSlider.value}px`;
+        transcriptionBox.style.fontSize = ${tailleTexteSlider.value}px;
     });
 } else {
     alert('La reconnaissance vocale n\'est pas supportée dans ce navigateur. Essayez avec Google Chrome.');
